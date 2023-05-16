093ba95df3059cc1399086fa2f4b0e5daa4a5617
Priyojit12 <96038868+Priyojit12@users.noreply.github.com>
2023-05-16 15:24:13 +0530

    Add files via upload

---
+++ reminder.py
@@ -0,0 +1,12 @@
+import time
+from plyer import notification
+def notify():
+    if __name__ == "__main__":
+        notification.notify(
+            title = "waterreminder",
+            message = "DRINK WATER FAST",
+            timeout = 1
+    )
+        time.sleep(36)
+        return notify()
+notify()
